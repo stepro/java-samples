d6a8066b033cc6cde44ee32b8311681f780cadeb
Stephen Provine <stephpr@microsoft.com>
2018-10-01 10:49:23 -0700

    Ensure e2e azds debugging works

--- a-module-with-dependencies/src/main/java/Main.java
+++ a-module-with-dependencies/src/main/java/Main.java
@@ -1,10 +1,12 @@
 import picocli.CommandLine;
 
 public final class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         for (String arg : args) {
             System.out.println(arg);
         }
         new CommandLine.Help.ColorScheme();
+        System.out.println("It worked!");
+        Thread.sleep(600000);
     }
 }
--- multi-module/app/src/main/java/Application.java
+++ multi-module/app/src/main/java/Application.java
@@ -1,8 +1,10 @@
 import picocli.CommandLine;
 
 public final class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Args.output(args);
         new CommandLine.Help.ColorScheme();
+        System.out.println("It worked!");
+        Thread.sleep(600000);
     }
 }
